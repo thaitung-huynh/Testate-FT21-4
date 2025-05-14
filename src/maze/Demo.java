01d8a7162cbbe1451b214a8d45978abc8d66d951
tung0410 <thaitung.mta@gmail.com>
2025-05-14 11:32:05 +0200

    Testate - FT21 - 4

--- src/maze/Demo.java
+++ src/maze/Demo.java
@@ -1,8 +1,11 @@
 package maze;
 
 public class Demo {
+    public static final String LS = System.lineSeparator();
     public static void main(String[] args) {
-        Maze m = new Maze(5, 5);
+        Maze m = Maze.parse("###########" + LS +
+                "#+ + + + +#" + LS +
+                "###########" + LS);
         System.out.println(m);
     }
 }
